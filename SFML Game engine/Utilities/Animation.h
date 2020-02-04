95d0c2cd54c35ee1a707ed0da53a654416d7f529
darienmiller88 <darienmiller88@yahoo.com>
2020-02-03 19:04:00 -0500

    Updated animations

--- SFML Game engine/Utilities/Animation.h
+++ SFML Game engine/Utilities/Animation.h
@@ -1,8 +1,25 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 
 class Animation{
 	public:
-		
+
+		/**
+		*@param textureFileName: The file name for the spritesheet
+		*@param spriteTimeLength: Time in seconds a singe sprite will stay on the screen.
+		*@param numSprites: vector detailing how many sprites are on the spritesheet horizontally (x), and vertically (y).
+		*/
+		Animation(const std::string &textureFileName, float spriteTimeLength, const sf::Vector2u &numSprites);
+		void updateSprites(float deltaTime);
+		void applyToSprite(sf::Sprite &sprite);
+
 	private:
+		sf::Clock clock;
+		sf::Texture spriteTexture;
+		std::vector<sf::IntRect> spriteRects;
+		int currentFrame;
+		float currentTime;
 		
+		//Time in seconds a singe sprite will stay on the screen.
+		const float SPRITE_LENGTH_TIME;
 };
